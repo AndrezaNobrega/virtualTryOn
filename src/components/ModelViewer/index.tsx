1a0371cc800dd3b0bbb3d661c175544e9544506c
AndrezaNobrega <ancarvalho@ecomp.uefs.br>
2026-03-12 21:55:23 -0300

    fix: improving perceived performance

--- src/components/ModelViewer/index.tsx
+++ src/components/ModelViewer/index.tsx
@@ -1,14 +1,35 @@
 import "./ModelViewer.css";
 import { Sparkle } from "@phosphor-icons/react";
+import { useState } from "react";
 
 type Props = {
   image: string;
+  blurImage?: string;
 };
 
-export function ModelViewer({ image }: Props) {
+export function ModelViewer({ image, blurImage }: Props) {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div className="modelViewer">
-      <img src={image} alt="Model preview" className="modelImage" />
+      <div className="modelImageWrapper">
+        {blurImage && (
+          <img
+            src={blurImage}
+            className="modelImageBlur"
+            alt=""
+            aria-hidden
+          />
+        )}
+
+        <img
+          src={image}
+          loading="lazy"
+          alt="Model preview"
+          className={`modelImage ${loaded ? "loaded" : ""}`}
+          onLoad={() => setLoaded(true)}
+        />
+      </div>
 
       <button className="tryOnButton">
         <Sparkle size={16} />
